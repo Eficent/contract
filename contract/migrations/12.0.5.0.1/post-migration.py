fabb50f96e2218ace915598cb9f3a64616948746
ahenriquez <ahenriquez@eficent.com>
2020-05-19 15:35:40 +0200

    fix migration scripts to be executed in 13

--- contract/migrations/12.0.5.0.1/post-migration.py
+++ contract/migrations/12.0.5.0.1/post-migration.py
@@ -1,4 +1,8 @@
 def migrate(cr, version):
+    cr.execute("""
+        alter table res_company
+        add column create_new_line_at_contract_line_renew boolean;
+    """)
     cr.execute(
         """\
             UPDATE res_company
